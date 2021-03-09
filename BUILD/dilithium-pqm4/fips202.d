a9e6bdfea434fba4dfd17fd265e5fe53b571d312
James Howe <jameshoweee@gmail.com>
2021-03-09 10:07:46 +0000

    dilithium stuff

---
+++ BUILD/dilithium-pqm4/fips202.d
@@ -0,0 +1,2 @@
+dilithium-pqm4/fips202.o: ../dilithium-pqm4/fips202.c .././mbed_config.h \
+ ../dilithium-pqm4/fips202.h ../dilithium-pqm4/keccakf1600.h
---
+++ BUILD/dilithium-pqm4/packing.d
@@ -0,0 +1,4 @@
+dilithium-pqm4/packing.o: ../dilithium-pqm4/packing.c .././mbed_config.h \
+ ../dilithium-pqm4/params.h ../dilithium-pqm4/config.h \
+ ../dilithium-pqm4/packing.h ../dilithium-pqm4/polyvec.h \
+ ../dilithium-pqm4/poly.h
