cf42389a305d7583d51e94e39958fb545caea93e
James Howe <jameshoweee@gmail.com>
2021-03-09 10:31:53 +0000

    dilithium stuff

--- BUILD/dilithium-pqm4/vector.d
+++
@@ -1,1 +0,0 @@
-dilithium-pqm4/vector.o: ../dilithium-pqm4/vector.s .././mbed_config.h
--- BUILD/falcon-20190918/shake.d
+++
@@ -1,3 +0,0 @@
-falcon-20190918/shake.o: ../falcon-20190918/shake.c .././mbed_config.h \
- ../falcon-20190918/inner.h ../falcon-20190918/config.h \
- ../falcon-20190918/fpr.h
